b64383d01271d3ea261fb68c733dece7f4622eba
Maan Singh <maan.vs97@gmail.com>
2020-05-07 23:09:19 -0700

    bobbyismahfriend

--- src/components/Bitcoin.js
+++ src/components/Bitcoin.js
@@ -1,6 +1,61 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import './Bitcoin.css';
+import LineChart from './LineChartBTC';
+import ToolTip from './ToolTip';
+import InfoBox from './InfoBox';
 
 class Bitcoin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchingData: true,
+      data: null,
+      hoverLoc: null,
+      activePoint: null,
+    };
+  }
+
+  handleChartHover(hoverLoc, activePoint) {
+    this.setState({
+      hoverLoc: hoverLoc,
+      activePoint: activePoint,
+    });
+  }
+
+  componentDidMount() {
+    const getData = () => {
+      const url = 'https://api.coindesk.com/v1/bpi/historical/close.json';
+
+      fetch(url)
+        .then((r) => r.json())
+        .then((bitcoinData) => {
+          const sortedData = [];
+          let count = 0;
+          for (let date in bitcoinData.bpi) {
+            sortedData.push({
+              d: moment(date).format('MMM DD'),
+              p: bitcoinData.bpi[date].toLocaleString('us-EN', {
+                style: 'currency',
+                currency: 'USD',
+              }),
+              x: count, //previous days
+              y: bitcoinData.bpi[date], // numerical price
+            });
+            count++;
+          }
+          this.setState({
+            data: sortedData,
+            fetchingData: false,
+          });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+    getData();
+  }
+
   render() {
     const registerStyle = {
       paddingTop: 20,
@@ -12,16 +67,46 @@ class Bitcoin extends Component {
         <div style={registerStyle}>
           <div className="card blue-grey darken-1">
             <div className="card-content white-text">
-              <span className="card-title">BTC</span>
-              <p>Bitcoin</p>
-              {/* <p className="right">
-            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
-          </p> */}
+              <span className="card-title center">BTCUSD</span>
+              <p className="center">Bitcoin 30 Day Price Chart</p>
+              <div className="container">
+                <div className="row">
+                  {/* <h1>30 Day Bitcoin Price Chart</h1> */}
+                </div>
+                <div className="row">
+                  {!this.state.fetchingData ? (
+                    <InfoBox data={this.state.data} />
+                  ) : null}
+                </div>
+                <div className="row">
+                  <div className="popup">
+                    {this.state.hoverLoc ? (
+                      <ToolTip
+                        hoverLoc={this.state.hoverLoc}
+                        activePoint={this.state.activePoint}
+                      />
+                    ) : null}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="chart">
+                    {!this.state.fetchingData ? (
+                      <LineChart
+                        data={this.state.data}
+                        onChartHover={(a, b) => this.handleChartHover(a, b)}
+                      />
+                    ) : null}
+                  </div>
+                </div>
+                <div className="row">
+                  <div id="coindesk">
+                    {' '}
+                    Powered by{' '}
+                    <a href="http://www.coindesk.com/price/">CoinDesk</a>
+                  </div>
+                </div>
+              </div>
             </div>
-            {/* <div className="card-action">
-          <a>Yes: {survey.yes}</a>
-          <a>No: {survey.no}</a>
-        </div> */}
           </div>
         </div>
       </div>
